d21d01b4a2932bdd17722bf0f055e25561fb7aae
yanitaBiheza <yanita.biheza@student.ehu.lt>
2024-09-09 23:21:57 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let str = '';
+    str = String(Math.abs(n));
+    let reversed = '';
+    reversed = str.split('').reverse().join('');
+    return Number(reversed);
 }
